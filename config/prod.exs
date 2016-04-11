e8652804f660982992a05e601789bf82e35dad1d
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-11 10:37:23 -0400

    heroku ready

--- config/prod.exs
+++ config/prod.exs
@@ -2,18 +2,16 @@ use Mix.Config
 
 config :chat, Chat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [  scheme: "https",
-          host: "secure-stream-82564.herokuapp.com",
-          port: 443],
-          force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "secure-stream-82564.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
 # Configure your database
-config :chat, Chat.Repo
+config :chat, Chat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20,
