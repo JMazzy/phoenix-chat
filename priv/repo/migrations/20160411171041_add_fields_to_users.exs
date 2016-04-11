eb20e97ceeeb9f2e15091dc927ab4f538b7dd626
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-11 13:15:03 -0400

    add fields to user

--- priv/repo/migrations/20160411171041_add_fields_to_users.exs
+++ priv/repo/migrations/20160411171041_add_fields_to_users.exs
@@ -2,5 +2,11 @@ defmodule Chat.Repo.Migrations.AddFieldsToUsers do
   use Ecto.Migration
 
   def change do
+
+    alter table(:users) do
+      add :username, :string
+      add :encrypted_password, :string
+    end
+
   end
 end
